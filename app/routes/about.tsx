ofk/react-router-github-pages

Implement about page

app/routes/about.tsx
@@ -1,0 +1,18 @@
+import { Link } from 'react-router';
+
+import type { Route } from './+types/about';
+
+export const meta: Route.MetaFunction = () => [{ title: 'About | New React Router App' }];
+
+export default function Home(): React.ReactElement {
+  return (
+    <div className="p-4">
+      <h1 className="text-4xl">About</h1>
+      <p>
+        <Link className="text-blue-600" to="/">
+          Welcome
+        </Link>
+      </p>
+    </div>
+  );
+}

ofk/example-react-router-hono-cdk

app/routes/about.tsx
@@ -1,0 +1,18 @@
+import { Link } from 'react-router';
+
+import type { Route } from './+types/about';
+
+export const meta: Route.MetaFunction = () => [{ title: 'About | New React Router App' }];
+
+export default function About(): React.ReactElement {
+  return (
+    <div className="p-4">
+      <h1 className="text-4xl">About</h1>
+      <p>
+        <Link className="text-blue-600" to="/">
+          Welcome
+        </Link>
+      </p>
+    </div>
+  );
+}