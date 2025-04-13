57d563acee67bb53ab5ee03323d9d5857b8a8431
ofk <ofkjpn+github@gmail.com>
2025-04-13 15:33:30 +0900

    Rename app to client

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import config from '@ofk/eslint-config-recommend';
 export default config({
   extends: [
     {
-      files: ['app/{root,routes}.*', 'app/routes/**'],
+      files: ['client/{root,routes}.*', 'client/routes/**'],
       rules: {
         'react-refresh/only-export-components': 'off',
       },
@@ -11,6 +11,6 @@ export default config({
   ],
   ignores: ['.react-router/', 'build/'],
   imports: {
-    defaultExportFiles: ['app/{root,routes}.*', 'app/routes/**'],
+    defaultExportFiles: ['client/{root,routes}.*', 'client/routes/**'],
   },
 });
--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
+  appDirectory: 'client',
   ssr: false,
 } satisfies Config;
