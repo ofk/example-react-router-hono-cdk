9e75de91f6e5cb7902e65250fb4f6b203eddc8e1
ofk <ofkjpn+github@gmail.com>
2025-04-20 01:34:29 +0900

    Update app-stack

--- infra/lib/app-stack.ts
+++ infra/lib/app-stack.ts
@@ -1,14 +1,39 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as apigatewayv2 from "aws-cdk-lib/aws-apigatewayv2";
+import * as integrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import path from "node:path";
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const apiHandler = new lambda.Function(this, "ApiLambda", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(
+        path.resolve(__dirname, "../../app/build/server")
+      ),
+    });
+
+    const httpApi = new apigatewayv2.HttpApi(this, "HttpApi", {
+      defaultIntegration: new integrations.HttpLambdaIntegration(
+        "LambdaIntegration",
+        apiHandler
+      ),
+    });
+
+    const apiOrigin = new origins.HttpOrigin(
+      cdk.Fn.parseDomainName(httpApi.apiEndpoint),
+      {
+        protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
+      }
+    );
 
     const siteBucket = new s3.Bucket(this, "SiteBucket", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -17,6 +42,15 @@ export class AppStack extends cdk.Stack {
 
     const distribution = new cloudfront.Distribution(this, "SiteDistribution", {
       defaultRootObject: "index.html",
+      additionalBehaviors: {
+        "api/*": {
+          origin: apiOrigin,
+          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      },
       defaultBehavior: {
         origin: origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
